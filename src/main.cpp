b0e3dd23a741d2df9608ec966410f03287a42060
Antonio Vanegas <hpsaturn@gmail.com>
2018-11-04 19:28:24 +0100

    some method refactors

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 #include <Wire.h>
 #include <MultiFuncShield.h>
 
-
 enum CountDownModeValues
 {
   COUNTING_STOPPED,
@@ -20,14 +19,12 @@ void setup() {
   Timer1.initialize();
   MFS.initialize(&Timer1);    // initialize multifunction shield library
   MFS.write(0);
-  
-  Serial.begin(9600);
 }
 
 void display (char min, char sec){
+  // display minutes and seconds sepated with a point
   MFS.write((float)((minutes*100 + seconds)/100.0),2);
 }
-
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -39,17 +36,13 @@ void loop() {
     case COUNTING_STOPPED:
         if (btn == BUTTON_1_SHORT_RELEASE && (minutes + seconds) > 0)
         {
-          // start the timer
-          countDownMode = COUNTING;
+          countDownMode = COUNTING; // start the timer
         }
         else if (btn == BUTTON_1_LONG_PRESSED)
         {
-          // reset the timer
-          tenths = 0;
+          tenths  = 0; // reset the timer
           seconds = 0;
           minutes = 0;
-
-          display(minutes,seconds);
         }
         else if (btn == BUTTON_2_PRESSED || btn == BUTTON_2_LONG_PRESSED)
         {
@@ -58,29 +51,26 @@ void loop() {
           {
             minutes = 0;
           }
-          display(minutes,seconds);
         }
         else if (btn == BUTTON_3_PRESSED || btn == BUTTON_3_LONG_PRESSED)
         {
-          seconds += 10;
+          seconds += 10; // continue counting down
           if (seconds >= 60)
           {
             seconds = 0;
           }
-          display(minutes,seconds);
         }
+        display(minutes,seconds);
         break;
         
     case COUNTING:
         if (btn == BUTTON_1_SHORT_RELEASE || btn == BUTTON_1_LONG_RELEASE)
         {
-          // stop the timer
-          countDownMode = COUNTING_STOPPED;
+          countDownMode = COUNTING_STOPPED; // stop the timer
         }
         else
-        {
-          // continue counting down
-          tenths++;
+        { 
+          tenths++; // continue counting down
           
           if (tenths == 10)
           {
